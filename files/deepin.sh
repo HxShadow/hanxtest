1eb6f659959c50d3ac2061ee451a23bad26674a3
hanx@lenovo <hanx.phy@foxmail.com>
2019-12-18 13:30:01 +0800

    rebuilding site Wed Dec 18 13:30:01 CST 2019

---
+++ files/deepin.sh
@@ -0,0 +1,8 @@
+# use Application store install QQ / sogou pinyin / xunlei / vlc / Shadowsocks-qt5 / goldendict / skype / evolution / GNU Image 
+
+# Root https://root.cern.ch/build-prerequisites
+sudo apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev
+sudo apt-get install gfortran libpcre3-dev libmysqlclient-dev libfftw3-dev libcfitsio-dev graphviz-dev libavahi-compat-libdnssd-dev  libldap2-dev python-dev libkrb5-dev libqt4-dev
+
+# OpenVPN
+sudo nmcli connection import type openvpn file hp.ovpn
